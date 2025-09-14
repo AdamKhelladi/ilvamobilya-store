978c65f94f411105592506bbe54d2903df4d0a35
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-14 16:12:40 +0100

    Add FixedBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import InfosBar from './components/InfosBar/InfosBar';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <InfosBar />
       <div className="container">
         <Navbar />
       </div>
