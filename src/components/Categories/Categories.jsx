0c83246b32a440a7f25a48e76e882e2119a31659
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-18 12:10:37 +0100

    Add Categories data

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,55 @@
 import "./Categories.css";
+import Image1 from "../../assets/category/category-img-4.png";
+import Image2 from "../../assets/category/category-img-2.png";
+import Image3 from "../../assets/category/category-img-3.png";
 
 export default function Categories() {
+  const categoryData = [
+    {
+      id: 1,
+      title: "Dining Table Chair",
+      type: "+5 TYPES",
+      description:
+        "Available in several models: 4 chairs and 6 chairs. Chair color of your choice.",
+      img: Image1,
+    },
+    {
+      id: 2,
+      title: "Sofas",
+      type: "+20 TYPES",
+      description:
+        "Available in several models: 2-seater, 3-seater, and corner sofa. Sofa color and material (fabric or leather) of your choice.",
+      img: Image2,
+    },
+    {
+      id: 3,
+      title: "Tables",
+      type: "+7 TYPES",
+      description:
+        "Elegant wooden coffee table designed to match your sofa. Available in different shapes and finishes to suit your style.",
+      img: Image3,
+    },
+  ];
+
   return (
-    <div className="categories-container">categories</div>
-  )
+    <div className="categories-container">
+      <div className="items">
+        {categoryData.map((item) => (
+          <div
+            key={item.id}
+            className={item.id === 1 ? "category-item wide" : "category-item"}
+          >
+            <div className="details">
+              <div className="types-number">{item.type}</div>
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+            <div className="category-img">
+              <img className={`img-${item.id}`} src={item.img} alt={item.title} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
