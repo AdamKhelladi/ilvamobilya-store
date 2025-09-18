9ae6b81bf1fe8ad9f9ae7ea3367007c79f72080a
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-18 15:39:45 +0100

    Add styles to Services component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,50 @@
 import "./Services.css";
+import { FiTruck } from "react-icons/fi";
+import { CiMoneyCheck1 } from "react-icons/ci";
+import { GoGift } from "react-icons/go";
+import { LiaTelegramPlane } from "react-icons/lia";
 
 export default function Services() {
+  const servicesData = [
+    {
+      id: 1,
+      icon: <FiTruck />,
+      title: "FREE SHIPPING",
+      description: "Free shipping on all orders across the country.",
+    },
+    {
+      id: 1,
+      icon: <CiMoneyCheck1 />,
+      title: "CASH ON DELIVERY",
+      description: "Pay easily at your doorstep with our cash on delivery option.",
+    },
+    {
+      id: 1,
+      icon: <GoGift />,
+      title: "GIFT CARDS",
+      description: "Buy gift cards and use coupon codes easily.",
+    },
+    {
+      id: 1,
+      icon: <LiaTelegramPlane />,
+      title: "CONTACT US",
+      description: "Keep in touch.",
+    },
+  ];
+
   return (
-    <div className="services-container">Services</div>
+    <div className="services-container">
+      {
+        servicesData.map((item) => (
+          <div key={item.id} className="service">
+            <div className="service-icon">{item.icon}</div>
+            <div className="details">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
