d5d212ceedede6c05166bb41bd977df9fd237988
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-18 14:55:43 +0100

    Add Services component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import { useState } from "react";
 import Menu from "./components/Navbar/Menu";
 import Hero from "./components/Hero/Hero";
 import Categories from "./components/Categories/Categories";
+import Services from "./components/Services/Services";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
 
   function handleShowMenu() {
-    setShowMenu(!showMenu);
+    const newState = !showMenu;
+    setShowMenu(newState);
+
+    if (newState) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
   }
 
   return (
     <div className="App">
       <Navbar handleShowMenu={handleShowMenu} />
-      {showMenu ? <Menu handleShowMenu={handleShowMenu} showMenu={showMenu}/> : null}
+      {showMenu ? (
+        <Menu handleShowMenu={handleShowMenu} showMenu={showMenu} />
+      ) : null}
 
       <div className="container">
         <Hero />
+        <Services />
         <Categories />
       </div>
     </div>
