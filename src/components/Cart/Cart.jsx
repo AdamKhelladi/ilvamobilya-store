b679a456a138df97659f52336d0a953802314f20
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-24 14:46:38 +0100

    Add Styles to payment card

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import "./Cart.css";
 import { IoMdClose } from "react-icons/io";
 
+import { IoPersonOutline } from "react-icons/io5";
+import { MdOutlineMail } from "react-icons/md";
+import { MdOutlinePhone } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+
 export default function Cart({ cartItems, deleteItem }) {
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    let total = cartItems.reduce((acc, item) => {
+      let clearPrice = item.price.replace(/[^0-9.]/g, "");
+      return acc + parseInt(clearPrice);
+    }, 0);
+
+    setTotalPrice(total);
+  }, [cartItems]);
+
   return (
     <div className="container">
       <div className="cart-container">
@@ -44,9 +61,98 @@ export default function Cart({ cartItems, deleteItem }) {
         </div>
         <div className="cart-right">
           <div className="total-price">
-            Total: <span>1999</span>$
+            Total Price: <span>{totalPrice}</span> DZD
           </div>
-          <div className="payment-info">Payment Info</div>
+          <div className="payment-info">
+            <h3>Payment Details</h3>
+
+            <div className="client-infos">
+              <form className="form" action="" method="POST">
+                <div>
+                  <IoPersonOutline className="icon" />
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Full name"
+                    // value={formData.name}
+                    // onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div>
+                  <MdOutlineMail className="icon" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    // value={formData.email}
+                    // onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div>
+                  <MdOutlinePhone className="icon" />
+                  <input
+                    type="tel"
+                    name="phone"
+                    placeholder="Phone number"
+                    // value={formData.phone}
+                    // onChange={handleChange}
+                    required
+                  />
+                </div>
+
+                <div>
+                  <IoLocationOutline className="icon" />
+                  <input
+                    type="text"
+                    name="address"
+                    placeholder="Address"
+                    // value={formData.address}
+                    // onChange={handleChange}
+                  />
+                </div>
+
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  // onChange={handleChange}
+                />
+
+                {/* {cartItems.map((item, index) => (
+                  <div key={index}>
+                    <input
+                      type="hidden"
+                      name={`product_${index + 1}_name`}
+                      value={item.name}
+                    />
+                    <input
+                      type="hidden"
+                      name={`product_${index + 1}_price`}
+                      value={item.price}
+                    />
+                  </div>
+                ))} */}
+
+                <input
+                  type="hidden"
+                  name="total_price"
+                  value={`${totalPrice}$`}
+                />
+
+                <button
+                  type="submit"
+                  className="form-btn"
+                  // disabled={!isFormValid}
+                  // onClick={handleSubmitBtn}
+                >
+                  Order Now
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
