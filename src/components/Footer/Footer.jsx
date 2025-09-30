9b1b4fd4e429db6704d2c0b1e6867c4ae09db994
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-30 11:01:25 +0100

    Fixing footer animation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
       initial={{ opacity: 0, y: 150 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: "easeOut" }}
-      viewport={{ once: false, amount: 0.2 }}
+      viewport={{ once: true, amount: 0.2 }}
     >
       <div className="container">
         <div className="content">
