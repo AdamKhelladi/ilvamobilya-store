8176d8ad1812c759b56b2780983547e08c139b8e
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-30 19:14:39 +0100

    test

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -52,6 +52,7 @@ export default function Hero() {
   return (
     <motion.div
       className="hero-container"
+      id="home"
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: "easeOut" }}
